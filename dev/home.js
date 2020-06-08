d91769f9fbc54b201898e2987cfaf671cd56f58c
Anna Dunajeva <anna.dunajeva@gmail.com>
2020-06-08 09:59:53 +0300

    removed scroll to bottom

--- dev/home.js
+++ dev/home.js
@@ -5,11 +5,7 @@ const viewport = document.querySelector(".viewport")
 
 const buttonAbout = document.querySelector(".button-about")
 
-const scrollableHight = viewport.scrollHeight 
 const windowHight = viewport.clientHeight
-let prevScrollY = viewport.scrollTop 
-let onBotton = scrollableHight - viewport.scrollTop === windowHight ? true : false
-let onTop = !onBotton
 
 const updatePreviousScrollYPosition = (scrollY) => {
     prevScrollY = scrollY
@@ -22,31 +18,4 @@ const setUpNavListeners = () => {
     })
 }
 
-const setUpScrollListeners = () => {
-    viewport.addEventListener('scroll', () => {
-        console.log('prevScrollY ', prevScrollY,'scrollY ', viewport.scrollTop, 'onBotton ', onBotton, onTop, scrollableHight-viewport.scrollTop, windowHight)
-        if (scrollableHight - viewport.scrollTop <= windowHight + 1) {
-            onBotton = true
-            onTop = false
-            updatePreviousScrollYPosition(viewport.scrollTop)
-        }
-        if (viewport.scrollTop === 0) {
-            onBotton = false
-            onTop = true
-            updatePreviousScrollYPosition(0)
-        }
-
-        if (viewport.scrollTop > prevScrollY + 10 && !onBotton) {
-            onBotton = true
-            onTop = false
-            scrollToPosition(viewport, windowHight) //idiotic solution to make sure it is scrolled to the bottom
-        } else if (viewport.scrollTop < prevScrollY - 10 && !onTop) {
-            onTop = true
-            onBotton = false
-            scrollToPosition(viewport, 0)
-        }
-    })
-}
-
-setUpScrollListeners()
 setUpNavListeners()
